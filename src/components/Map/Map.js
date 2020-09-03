0cc6ffadb081fa34156994cdb2488c423313a3c1
Mateusz Kłysz <mateuszklysz@outlook.com>
2020-09-03 10:56:29 +0200

    change map provider

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,27 @@
-import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
-const mapContainer = {
+const mapSettings = {
+  mapboxApiAccessToken: process.env.GATSBY_MAPS_API,
+  mapStyle: "mapbox://styles/nickyy00/ckemjqtdj1cje19o2e2t60ry6",
   width: "500px",
   height: "300px",
 };
 
 const Map = ({ data }) => {
+  const [viewport, setViewport] = useState({
+    latitude: data.coord.lat,
+    longitude: data.coord.lon,
+    zoom: 11,
+    pitch: 55.0,
+  });
+
   return (
-    <LoadScript googleMapsApiKey={process.env.GATSBY_MAPS_API}>
-      <GoogleMap
-        mapContainerStyle={mapContainer}
-        zoom={11}
-        center={{ lat: data.coord.lat, lng: data.coord.lon }}
-        options={{ disableDefaultUI: true }}
-      ></GoogleMap>
-    </LoadScript>
+    <ReactMapGL
+      {...mapSettings}
+      {...viewport}
+      onViewportChange={viewport => setViewport(viewport)}
+    ></ReactMapGL>
   );
 };
 
