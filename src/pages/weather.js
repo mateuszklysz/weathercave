9007a5ce57916af4a1873671c8b6e7cac40f9cd9
Mateusz Kłysz <mateuszklysz@outlook.com>
2020-09-06 23:16:37 +0200

    add / animate notFound

--- src/pages/weather.js
+++ src/pages/weather.js
@@ -27,23 +27,24 @@ const Weather = props => {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const tl = gsap.timeline();
-    tl.fromTo(
-      [containerRef.current, ".shapesContainer"],
-      { autoAlpha: 0 },
-      { autoAlpha: 1, duration: 2, stagger: 2, ease: "Power1.easeInOut" }
-    ).fromTo(
-      ".shapesContainer div",
-      { x: -50 },
-      { x: 0, duration: 3, stagger: 0.1, ease: "Power1.easeInOut" }
-    );
-  }, []);
+    if (found) {
+      const tl = gsap.timeline();
+      tl.fromTo(
+        [containerRef.current, ".shapesContainer"],
+        { autoAlpha: 0 },
+        { autoAlpha: 1, duration: 2, stagger: 2, ease: "Power1.easeInOut" }
+      ).fromTo(
+        ".shapesContainer div",
+        { x: -50 },
+        { x: 0, duration: 3, stagger: 0.1, ease: "Power1.easeInOut" }
+      );
+    }
+  }, [found]);
 
   useEffect(() => {
-    gsap.to(
-      [containerRef.current.children[1], containerRef.current.children[2]],
-      { y: 15, autoAlpha: 0 }
-    );
+    if (found) {
+      gsap.to(containerRef.current.children[1], { y: 15, autoAlpha: 0 });
+    }
 
     axios
       .get(
@@ -56,85 +57,103 @@ const Weather = props => {
       })
       .then(() => {
         setFound(true);
-      })
-      .catch(() => {
-        setFound(false);
-      })
-      .then(() => {
         gsap.to(containerRef.current.children, {
           y: 0,
           autoAlpha: 1,
         });
+      })
+      .catch(() => {
+        setFound(false);
+        gsap.fromTo(
+          containerRef.current.children[1],
+          {
+            x: -10,
+            autoAlpha: 0,
+          },
+          {
+            x: 0,
+            autoAlpha: 1,
+          }
+        );
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputData[0]]);
 
-  return (
-    <>
+  if (found) {
+    return (
+      <>
+        <StyledContainer ref={containerRef}>
+          <NavBar />
+          <WeatherComponent data={result} />
+        </StyledContainer>
+        <div className="shapesContainer">
+          <Shape
+            width="35%"
+            height="10%"
+            top="10%"
+            right="0"
+            opacity="30%"
+            blue={true}
+          />
+          <Shape
+            width="45%"
+            height="20%"
+            top="0"
+            right="0"
+            opacity="20%"
+            blue={true}
+          />
+          <Shape
+            width="7%"
+            height="25%"
+            top="15%"
+            left="0"
+            opacity="60%"
+            blue={true}
+          />
+          <Shape
+            width="12%"
+            height="30%"
+            bottom="3%"
+            left="3%"
+            opacity="20%"
+            blue={true}
+          />
+          <Shape
+            width="50%"
+            height="20%"
+            bottom="0"
+            left="40%"
+            opacity="20%"
+            blue={true}
+          />
+          <Shape
+            width="20%"
+            height="20%"
+            top="40%"
+            right="15%"
+            opacity="20%"
+            blue={true}
+          />
+          <Shape
+            width="27%"
+            height="30%"
+            top="27%"
+            left="30%"
+            opacity="30%"
+            blue={true}
+          />
+        </div>
+      </>
+    );
+  } else {
+    return (
       <StyledContainer ref={containerRef}>
         <NavBar />
-        {found ? <WeatherComponent data={result} /> : <NotFound />}
+        <NotFound />
       </StyledContainer>
-      <div className="shapesContainer">
-        <Shape
-          width="35%"
-          height="10%"
-          top="10%"
-          right="0"
-          opacity="30%"
-          blue={true}
-        />
-        <Shape
-          width="45%"
-          height="20%"
-          top="0"
-          right="0"
-          opacity="20%"
-          blue={true}
-        />
-        <Shape
-          width="7%"
-          height="25%"
-          top="15%"
-          left="0"
-          opacity="60%"
-          blue={true}
-        />
-        <Shape
-          width="12%"
-          height="30%"
-          bottom="3%"
-          left="3%"
-          opacity="20%"
-          blue={true}
-        />
-        <Shape
-          width="50%"
-          height="20%"
-          bottom="0"
-          left="40%"
-          opacity="20%"
-          blue={true}
-        />
-        <Shape
-          width="20%"
-          height="20%"
-          top="40%"
-          right="15%"
-          opacity="20%"
-          blue={true}
-        />
-        <Shape
-          width="27%"
-          height="30%"
-          top="27%"
-          left="30%"
-          opacity="30%"
-          blue={true}
-        />
-      </div>
-    </>
-  );
+    );
+  }
 };
 
 export default Weather;
